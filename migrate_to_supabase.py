9a61133e68ea9fbf8771f6881fe6adb288fb660e
pd0203 <pd0203@naver.com>
2026-01-21 00:43:22 +0900

    EDIT: 상품원가표 데이터 일부 수정2

--- migrate_to_supabase.py
+++ migrate_to_supabase.py
@@ -19,6 +19,9 @@ import os
 import sys
 import json
 from datetime import date
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SUPABASE_URL = os.environ.get('SUPABASE_URL', '')
 SUPABASE_KEY = os.environ.get('SUPABASE_KEY', '')
